7ab272bb6561da6ca24339012938a96b492d3df4
VasiliyOs <vasiliy2809_ne@mail.ru>
2024-12-03 13:28:10 +0300

    Delete using namespace std

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
+
 int main() {
     string name;
-    std::cout<<"Введите имя пользователя: ";
+    std::cout<<"Введите имя пользователя:";
     std::cin>>name;
     std::cout << "Hello world from "<<name<<std::endl;
     return 0;
